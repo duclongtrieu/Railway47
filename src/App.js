db5d1b487daaba8d6983b2eca19c781146336165
duclongtrieu <duclongtrieu@gmail.com>
2022-10-16 18:39:40 +0700

    project 47

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./App.css";
+// import React from "react";
+
+//Q1
+// const divstyle = {
+//   color: " pink",
+// };
+// function App() {
+//   return React.createElement("h1", { style: divstyle }, "Hello Word");
+// }
+
+//Q2(a)
+// function App(){
+//   return(
+//     <div>
+//       <h1> Hello World</h1>
+//       <p>  My frist paragraph</p>
+//     </div>
+//   );
+// }
+
+//Q2(b)
+// function App() {
+//   return (
+//     <div>
+//       <th> DANH SÁCH LỚP HỌC </th>
+//       <ol>
+//         <li>Nguyễn Ngọc Duy</li>
+//         <li>Tống Quang Anh</li>
+//         <li>Triệu Đức Long</li>
+//         <li>Đinh Thu Loan</li>
+//         <li>Nguyễn Tiến Quang</li>
+//       </ol>
+//       <br />
+//       <ol>
+//         <li>
+//           {" "}
+//           Cơm trưa
+//           <ul>Cơm chiên hải sản</ul>
+//           <ul>Cơm canh chua cá lóc</ul>
+//         </li>
+//         <li>
+//           Tráng miệng trái cây
+//           <ul>Nho</ul>
+//           <ul>Xoài</ul>
+//         </li>
+//       </ol>
+//     </div>
+//   );
+// }
+
+//Q3(c)
+// const css = {
+//   color: "pink",
+//   margin: "40px",
+//   padding: "30px",
+// };
+
+// const handleshow = () => {
+//   alert("success");
+// };
+// function App() {
+//   return (
+//     <div style={css} onClick={handleshow}>
+//       {" "}
+//       Hellooooooo
+//     </div>
+//   );
+// }
+export default App;
